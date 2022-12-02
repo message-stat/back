652cfd58e7a0acc02522255112cd9617d5ad97cc
Andrei <soprachev@mail.ru>
2022-12-02 23:04:39 +0300

    cicd

--- src/db/up.ts
+++ src/db/up.ts
@@ -2,11 +2,15 @@
 
 export const up = `
 create table if not exists Word (
-  dateTime DateTime64,
-  text String,
-  userId FixedString(64),
-  position Enum8('first' = 0, 'begin' = 1, 'center' = 2, 'end' = 3, 'last' = 4),
-  messageLength Enum8('single' = 0, 'short' = 1, 'medium' = 2, 'long' = 3)
+  dateTime      DateTime64,
+  text          String,
+  userId        FixedString(64),
+  position      Enum8('first' = 0, 'begin' = 1, 'center' = 2, 'end' = 3, 'last' = 4),
+  messageLength Enum8('single' = 0, 'short' = 1, 'medium' = 2, 'long' = 3),
+  lang          Enum8('ru' = 0, 'en' = 1) MATERIALIZED if(length(extract(text, '([а-я|ё]+)')) > 0, 'ru', 'en'),
+  lemma         String MATERIALIZED if(lang = 'ru', lemmatize('ru', text), lemmatize('en', text)),
+  stem          String MATERIALIZED if(lang = 'ru', stem('ru', lemma), stem('en', lemma)),
+  debug         String
 ) engine = MergeTree()
   order by (userId, intHash32(toUInt32(dateTime)))
   partition by userId
